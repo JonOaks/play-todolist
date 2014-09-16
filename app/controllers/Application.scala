2ec9da7ed8c644b0040dc17aa48b6765e88638de
Jonatan Diego Poyato Hernández <jdph1@alu.ua.es>
2014-09-17 00:26:10 +0200

    Creado formulario simple

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,11 +3,18 @@ package controllers
 import play.api._
 import play.api.mvc._
 
+import play.api.data._
+import play.api.data.Forms._
+
 object Application extends Controller {
 
   def index = Action {
     Redirect(routes.Application.tasks)
   }
+
+   val taskForm = Form(
+      "label" -> nonEmptyText
+   )
 
   def tasks = TODO
   def newTask = TODO
