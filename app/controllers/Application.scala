0fca4ecaaed41efc9f654f8ca70ad60d76377a44
Jonatan Diego <jdph1@alu.ua.es>
2014-10-02 15:13:12 +0200

    Recolocación métodos en Application

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -14,6 +14,13 @@ object Application extends Controller {
 
    val html: String = "<html><head><style>body {background-color: #daecee;}#error {position: absolute;top: 50%;left: 50%;margin-top: -303px;margin-left: -303px;}</style></head><body><div id=\"error\"><a href=\"http://huwshimi.com/\"><img src=\"http://huwshimi.com/wp-content/themes/huwshimi/images/404.png\" alt=\"404 page not found\" id=\"error404-image\"></a></div></body></html>"
 
+   implicit val json = new Writes[Task] {
+      def writes(task: Task) = Json.obj (
+         "id" -> task.id,
+         "label" -> task.label
+         )
+   }
+
    def index = Action {
       Redirect(routes.Application.tasks)
    }
@@ -36,23 +43,16 @@ object Application extends Controller {
       )
    }
 
-   def deleteTask(id: Long) = Action {
-      Task.delete(id)
-      Redirect(routes.Application.tasks)
-   }
-
-   implicit val json = new Writes[Task] {
-      def writes(task: Task) = Json.obj (
-         "id" -> task.id,
-         "label" -> task.label
-         )
-   }
-
    def getTask(id: Long) = Action {
       Task.task(id) match {
          case None => NotFound(html).as("text/html")
          case Some(t) => Ok(Json.toJson(t))
       }
+   }
+
+   def deleteTask(id: Long) = Action {
+      Task.delete(id)
+      Redirect(routes.Application.tasks)
    }
 }
 
