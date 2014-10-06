fe89bb824964ba865d84f94b6dc1dec9000d8b24
Jonatan Diego <jdph1@alu.ua.es>
2014-10-07 00:25:06 +0200

    Actualizado código de tasks en base a los nuevos requisitos implementado conversor implícito nuevo, antiguo comentado

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -9,17 +9,23 @@ import play.api.data.Forms._
 import models.Task
 
 import play.api.libs.json._
+import play.api.libs.functional.syntax._
 
 object Application extends Controller {
 
    val html: String = "<html><head><style>body {background-color: #daecee;}#error {position: absolute;top: 50%;left: 50%;margin-top: -303px;margin-left: -303px;}</style></head><body><div id=\"error\"><a href=\"http://huwshimi.com/\"><img src=\"http://huwshimi.com/wp-content/themes/huwshimi/images/404.png\" alt=\"404 page not found\" id=\"error404-image\"></a></div></body></html>"
 
-   implicit val json = new Writes[Task] {
+   /*implicit val json = new Writes[Task] {
       def writes(task: Task) = Json.obj (
          "id" -> task.id,
          "label" -> task.label
          )
-   }
+   }*/
+
+   implicit val taskWrites: Writes[Task] = (
+      (JsPath \ "id").write[Long] and
+      (JsPath \ "label").write[String]
+   )(unlift(Task.unapply))
 
    def index = Action {
       Redirect(routes.Application.tasks)
@@ -30,7 +36,8 @@ object Application extends Controller {
    )
 
    def tasks = Action {
-      Ok(views.html.index(Task.all(), taskForm))
+      val json = Json.toJson(Task.all())
+      Ok(json)
    }
 
    def newTask = Action { 
