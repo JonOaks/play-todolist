b0120420f5d3941ef6c12c9b02e3050cd80dfb7d
Jonatan Diego <jdph1@alu.ua.es>
2014-11-04 16:08:30 +0100

    Añadida prueba: consulta de una tarea existente

--- test/TaskSpec.scala
+++ test/TaskSpec.scala
@@ -7,6 +7,15 @@ import org.junit.runner._
 import play.api.test._
 import play.api.test.Helpers._
 
+import models.Task
+
 class TaskSpec extends Specification {
-   
+   "Tasks" should{
+      "return task" in {
+         running(FakeApplication()){
+            val task = Task.task(1)
+            task must beSome
+         }
+      }
+   }
 }
