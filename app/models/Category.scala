b5f6b97d50450e3733f2774b1e82ec152298a57d
Jonatan Diego <jdph1@alu.ua.es>
2014-11-10 17:44:59 +0100

    Implementado nuevo método en el modelo Category sirve para comprobar si la categoría especificada pertenece a un usuario dado

--- app/models/Category.scala
+++ app/models/Category.scala
@@ -20,4 +20,15 @@ object Category {
    def existCategory(category_name: String): Option[Category] = DB.withConnection{
       implicit c => SQL("select * from category where category = {category_name}").on('category_name -> category_name).as(category.singleOpt)
    }
+
+   def categoryBelongToUser(category_name: String, login: String): Long = {
+      var cantidad = 0L
+      val rowParser = scalar[Long]
+      val rsParser = scalar[Long].single
+
+      DB.withConnection {implicit c => cantidad = SQL("select count(*) from user_category where category = {category_name} and login = {login}").on('login -> login).on('category_name -> category_name).as(scalar[Long].single)
+      }
+
+      return cantidad
+   }
 }
